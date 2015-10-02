8954f27e1b621df16415fdd589af2ce7b9ef28ea
zdgeorgiev <zddavko@gmail.com>
2015-10-03 00:35:49 +0300

    fixed

---
+++ Breakout/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breakout
+{
+    public class Engine
+    {
+        public Engine()
+        {
+
+        }
+
+        /// <summary>
+        /// Where the game starts
+        /// </summary>
+        public void Start()
+        {
+
+        }
+    }
+}
