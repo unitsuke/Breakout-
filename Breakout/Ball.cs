89d53ff5137a4e0e81b7ab2e723b3a766f10a51f
sokito <sokratovgeorgi.w@gmail.com>
2015-10-03 16:05:17 +0300

    ballClass

--- Breakout/Ball.cs
+++ Breakout/Ball.cs
@@ -8,10 +8,20 @@ namespace Breakout
     public class Ball
     {
         static char charBall = '@';
-        public int Ball { get ; set; }
-        public Ball()
-        {
+        
+        //properties
+        public Point BallPoint { get ; set; }
+        public Point Direction { get; set; }
+        public int Damage { get; set; }
 
+
+        //constructor
+        public Ball(Point point, int damage)
+        {
+            this.BallPoint = point;
+            this.Damage = damage;
         }
+        
+
     }
 }
