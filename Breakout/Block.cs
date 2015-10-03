911dd4e433c362a0b554e1294615c1fb9b7d136f
sokito <sokratovgeorgi.w@gmail.com>
2015-10-03 16:56:28 +0300

    BlockCreate

--- Breakout/Block.cs
+++ Breakout/Block.cs
@@ -7,5 +7,18 @@ namespace Breakout
 {
     public class Block
     {
+        static char charBlock = '#';
+        //properties
+        public Point BlockPoint { get; set; }
+        public int Life { get; set; }
+        public int Score { get; set; }
+
+        //constructor
+        public Block(Point point,int life, int score)
+        {
+            this.BlockPoint = point;
+            this.Life = life;
+            this.Score = score;
+        }
     }
 }
